b9c727f8617d82b073d26064d635b2e3674cf200
Anton Kesy <anton@kesy.de>
2022-06-05 21:02:34 +0200

    Create IMinimumCollection.hpp

---
+++ include/minimum/collections/IMinimumCollection.hpp
@@ -0,0 +1,29 @@
+#ifndef PRIORITY_QUEUE_IMINIMUMCOLLECTION_HPP
+#define PRIORITY_QUEUE_IMINIMUMCOLLECTION_HPP
+
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include "../../IPriorityQueue.hpp"
+
+namespace priority_queues::minimum {
+
+    using priority_queues::IKey;
+
+    template<typename T, typename K>
+    class IMinimumCollection {
+    public:
+        virtual bool push(std::shared_ptr<IKey<K>> key, std::shared_ptr<T> value) = 0;
+
+        virtual std::shared_ptr<T> top() = 0;
+
+        virtual std::shared_ptr<T> pop() = 0;
+
+        virtual void decreaseKey(std::size_t &key_to_decrease, std::shared_ptr<IKey<K>> new_key_value) = 0;
+
+        virtual std::optional<std::size_t> index_of(std::shared_ptr<IKey<K>> key_to_find) = 0;
+    };
+
+}
+
+#endif //PRIORITY_QUEUE_IMINIMUMCOLLECTION_HPP
