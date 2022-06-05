7813f3e0ccc8e78b0c6cb5c91014c9bac3eadbd3
Anton Kesy <anton@kesy.de>
2022-06-05 21:02:22 +0200

    Update IPriorityQueue.hpp

--- include/IPriorityQueue.hpp
+++ include/IPriorityQueue.hpp
@@ -2,19 +2,33 @@
 #define PRIORITY_QUEUE_IPRIORITYQUEUE_HPP
 
 #include <cstddef>
+#include <memory>
 
-namespace priority_queue {
+namespace priority_queues {
 
-    template<class T>
+    template<typename T>
+    class IKey {
+    public:
+        virtual int compareTo(IKey &other) = 0;
+
+        virtual T getValue() = 0;
+
+        virtual bool operator<(IKey<T> &other) = 0;
+
+        virtual bool operator>(IKey<T> &other) = 0;
+
+    };
+
+    template<typename T, typename K>
     class IPriorityQueue {
     public:
-        virtual bool push(std::size_t key, T *value) = 0;
+        virtual bool push(IKey<K> &key, T &value) = 0;
 
-        virtual T *top() = 0;
+        virtual std::shared_ptr<T> top() = 0;
 
-        virtual T *pop() = 0;
+        virtual std::shared_ptr<T> pop() = 0;
 
-        virtual bool decreaseKey(std::size_t key_to_decrease, std::size_t new_key_value) = 0;
+        virtual void decreaseKey(IKey<K> &key_to_decrease, IKey<K> &newKey) = 0;
     };
 }
 
